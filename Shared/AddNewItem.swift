edae3e1dd2688c198ee396943a4acc97ffc33ae5
WZYCX <chenalator2004@gmail.com>
2022-08-16 12:14:11 +0100

    Added new Page for 'Add New Item' in which a new item in stock can be added to the Firestore DB.

---
+++ Shared/AddNewItem.swift
@@ -0,0 +1,29 @@
+//
+//  AddNewItem.swift
+//  Rugby School Stodge STAFF
+//
+//  Created by William Chen on 07/08/2022.
+//
+
+import SwiftUI
+
+struct AddNewItemPage: View {
+    var body: some View {
+        VStack{
+            Header()
+            
+            Text("Add New Item")//placeholder for "Hi \n\(name)"
+                .font(.system(size: 50, weight: .semibold))
+            
+            Spacer()
+        }
+    }
+}
+
+
+
+struct AddNewItem_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewItemPage()
+    }
+}
